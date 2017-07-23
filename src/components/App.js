2439d9b9eff624ec5df80fc934decb355212163a
ja-ho <magicuv@gmail.com>
2017-07-23 19:03:14 +0900

    yoojin

--- src/components/App.js
+++ src/components/App.js
@@ -19,12 +19,17 @@ class Contacts extends Component{
 				{name:"charlie", phone:"010-0000-0003"},
 				{name:"licole", phone:"010-0000-0004"}
 			],
-			selectedKey: -1
+			selectedKey: -1,
+			selected: {
+				name: "",
+				phone: ""
+			}
 		};
 		this._insertContact=this._insertContact.bind(this);
 		this._isSelected=this._isSelected.bind(this);
 		this._onSelect=this._onSelect.bind(this);
 		this._removeContact=this._removeContact.bind(this);
+		this._editContact=this._editContact.bind(this);
 	}
 
 	_insertContact(name, phone){
@@ -40,12 +45,17 @@ class Contacts extends Component{
 		if(key==this.state.selectedKey) {
 			console.log("key select Cancelled");
 			this.setState({
-				selectedKey: -1
+				selectedKey: -1,
+				selected: {
+					name: "",
+					phone: ""
+				}
 			});
 			return;
 		}
 		this.setState({
-			selectedKey: key
+			selectedKey: key,
+			selected: this.state.contactData[key]
 		});
 		console.log(key + " is selected");
 	}
@@ -76,6 +86,24 @@ class Contacts extends Component{
 		});
 	}
 
+	_editContact(name, phone) {
+		this.setState({
+			contactData: update(
+				this.state.contactData,
+				{
+					[this.state.selectedKey]: {
+						name: {$set: name},
+						phone: {$set: phone}
+					}
+				}
+			),
+			selected: {
+				name: name,
+				phone: phone
+			}
+		});
+	}
+
 	render() {
 		return(
 			<div>
@@ -93,6 +121,9 @@ class Contacts extends Component{
 				</ul>
 				<ContactCreator onInsert={this._insertContact}/>
 				<ContactRemover onRemove={this._removeContact}/>
+				<ContactEditor onEdit={this._editContact}
+							   isSelected={(this.state.selectedKey != -1)}
+							   contact={this.state.selected}/>
 			</div>
 		);
 	}
@@ -104,7 +135,11 @@ class ContactInfo extends Component {
 		this.props.onSelect(this.props.contactKey);
 	}
 
+	shouldComponentUpdate(nextProps, nextState) {
+		return (JSON.stringify(nextProps) != JSON.stringify(this.props));
+	}
 	render() {
+		console.log("rendered" + this.props.name);
 		let getStyle = (isSelect) => {
 			if(!isSelect) return;
 
@@ -119,7 +154,6 @@ class ContactInfo extends Component {
 			<li style={getStyle(this.props.isSelected)}
 				onClick={this.handleClick.bind(this)}>{this.props.name} {this.props.phone}
 			</li>
-
 		);
 	}
 
@@ -183,6 +217,62 @@ class ContactRemover extends Component {
 			<button onClick={this.handleClick.bind(this)}>
 				Remove selected contact
 			</button>
+		);
+	}
+}
+
+class ContactEditor extends Component {
+	constructor(props) {
+		super(props);
+		//Configure default state
+		this.state = {
+			name:"",
+			phone:""
+		};
+		this.handleChange=this.handleChange.bind(this);
+		this.handleClick=this.handleClick.bind(this);
+	}
+
+	handleClick(){
+		if(!this.props.isSelected) {
+			console.log("contact not selected");
+			return;
+		}
+		this.props.onEdit(this.state.name, this.state.phone);
+	}
+
+	handleChange(e) {
+		var nextState={};
+		nextState[e.target.name] = e.target.value;
+		this.setState(nextState);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			name: nextProps.contact.name,
+			phone: nextProps.contact.phone
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<p>
+					<input type="text"
+						   name="name"
+						   placeholder="name"
+						   value={this.state.name}
+						   onChange={this.handleChange} />
+					<input type="text"
+						   name="phone"
+						   placeholder="phone"
+						   value={this.state.phone}
+						   onChange={this.handleChange} />
+					<button onClick={this.handleClick}>
+                    Edit
+                    </button>
+                </p>
+            </div>
 		);
 	}
 }
